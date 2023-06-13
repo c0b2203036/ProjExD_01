1bd7999f706e5a5686d5c087bb49023b9eef9a8d
takuoba <c0b2203036@edu.teu.ac.jp>
2023-06-13 16:10:48 +0900

    練習６

--- pygame_image.py
+++ pygame_image.py
@@ -4,7 +4,7 @@ import pygame as pg
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((800, 600))
+    screen = pg.display.set_mode((1600, 900))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01-20230613/fig/pg_bg.jpg")
     kk_img01 = pg.image.load("ex01-20230613/fig/3.png")
@@ -16,11 +16,16 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [-tmr, 0])
+        screen.blit(bg_img, [1600-tmr, 0])
         screen.blit(kk_imgs[tmr%2], [300,200])
+        
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(100)
+
+        if tmr == 1599:
+            tmr = 0
 
 
 if __name__ == "__main__":
