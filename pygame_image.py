0c6f9a686262ec97872b55cec06812867bf6b79e
takuoba <c0b2203036@edu.teu.ac.jp>
2023-06-13 17:18:21 +0900

    課題１

--- pygame_image.py
+++ pygame_image.py
@@ -4,29 +4,27 @@ import pygame as pg
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((1600, 900))
+    screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("ex01-20230613/fig/pg_bg.jpg")
+    bg_img01 = pg.image.load("ex01-20230613/fig/pg_bg.jpg")
+    bg_img02 = pg.transform.flip(bg_img01,True,False)
     kk_img01 = pg.image.load("ex01-20230613/fig/3.png")
     kk_img01 = pg.transform.flip(kk_img01,True,False)
-    kk_img02 = pg.transform.rotozoom(kk_img01,10,1.0)
-    kk_imgs = [kk_img01, kk_img02]
+    kk_imgs = [kk_img01, pg.transform.rotozoom(kk_img01,6,1.0),pg.transform.rotozoom(kk_img01,10,1.0),pg.transform.rotozoom(kk_img01,4,1.0)]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [-tmr, 0])
-        screen.blit(bg_img, [1600-tmr, 0])
-        screen.blit(kk_imgs[tmr%2], [300,200])
         
+        screen.blit(bg_img01, [-tmr, 0])
+        screen.blit(bg_img02, [1600-tmr, 0])
+        screen.blit(bg_img01, [3200-tmr, 0])
+        screen.blit(kk_imgs[tmr%200//50], [300,200])
         pg.display.update()
         tmr += 1        
-        clock.tick(100)
-
-        if tmr == 1599:
-            tmr = 0
-
+        clock.tick(200)
+        if(tmr>3199):
+            tmr=0
 
 if __name__ == "__main__":
     pg.init()
